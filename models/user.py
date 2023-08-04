0ff7911d53463a1dab056ef12c0881d242b3539d
semaed <figsaned@gmail.com>
2023-08-03 22:17:32 -0400

    Corrections

--- models/user.py
+++ models/user.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/pyhton3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
@@ -26,32 +26,5 @@ class User(BaseModel, Base):
     reviews = relationship(
         'Review',
         backref=backref('user', cascade='all, delete-orphan'),
-        cascade='all, delete-orphan'
-    )
-
-
-class Place(BaseModel, Base):
-    """ A place to stay """
-    __tablename__ = 'places'
-
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024))
-    number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, default=0)
-    max_guest = Column(Integer, nullable=False, default=0)
-    price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float)
-    longitude = Column(Float)
-
-    # Relationship with the User class
-    user = relationship('User', backref=backref(
-        'places', cascade='all, delete-orphan'))
-
-    # Relationship with the Review class
-    reviews = relationship(
-        'Review',
-        backref=backref('place', cascade='all, delete-orphan'),
         cascade='all, delete-orphan'
     )
