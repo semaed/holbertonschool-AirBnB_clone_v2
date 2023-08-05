15b83efa1143cffe2e16e8048f2582f96c81b7d2
semaed <figsaned@gmail.com>
2023-08-04 22:28:00 -0400

    Update

--- models/amenity.py
+++ models/amenity.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
-from models.place import place_amenity
+from models.base_model import BaseModel
+from models.base_model import Base
+from os import getenv
+from sqlalchemy import Column, String
 
 
 class Amenity(BaseModel, Base):
-    """Class that inherits frm basemodel and base"""
-
-    __tablename__ = "ameinities"
-
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity)
-    user_id = Column(String(60), nullable=False, ForeignKey="users.id")
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
